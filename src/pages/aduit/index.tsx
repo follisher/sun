e3eafd2d2cbf9a661637cfe36e2daa492a1e597b
SvenMingze <39316450@qq.com>
2024-09-10 22:56:00 +0800

    [feat]: 添加数据列

--- src/pages/aduit/index.tsx
+++ src/pages/aduit/index.tsx
@@ -22,11 +22,20 @@ const Audit: React.FC = () => {
     <>
       <Table
         size="small"
-        dataSource={list}
+        dataSource={list.map((item, index) => {
+          return {
+            ...item,
+            key: index + 1,
+            wsh: item.wsh === null || item.wsh === undefined ? 0 : item.wsh,
+            sh: item.sh === null || item.sh === undefined ? 0 : item.sh,
+            sb: item.sb === null || item.sb === undefined ? 0 : item.sb,
+            fhth: item.fhth === null || item.fhth === undefined ? 0 : item.fhth,
+          };
+        })}        
         columns={[
           {
             title: "序号",
-            dataIndex: "",
+            dataIndex: "key",
           },
           {
             title: "日期",
@@ -35,9 +44,13 @@ const Audit: React.FC = () => {
           {
             title: "未审核",
             dataIndex: "wsh",
+          },          
+          {
+            title: "已审核",
+            dataIndex: "sh",
           },
           {
-            title: "待上报",
+            title: "已审核并上报",
             dataIndex: "sb",
           },
           {
